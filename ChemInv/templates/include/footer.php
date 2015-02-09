e8d7ec95abbcc6a04f69e0a09bfb0318843cc199
Alec Bellati <alec.bellati@student.adelaide.edu.au>
2015-02-09 14:44:33 +1030

    Add page footer and administrator schema.

--- ChemInv/templates/include/footer.php
+++ ChemInv/templates/include/footer.php
@@ -1,3 +1,16 @@
 		</div>
+		<div id="footer">
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			&copy 2015, School of Chemical Engineering, The University of Adelaide
+			<br />
+			<a href="<?php echo ROOT_PATH;?>chemsearch/">Administrator</a><br />
+		</div>
 	</body>
 </html>
