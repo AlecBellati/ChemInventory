5117fd7cf2348ccd1a56116affb093aa8ec96f68
Alec Bellati <alec.bellati@student.adelaide.edu.au>
2015-02-13 14:37:21 +1030

    Fix small config bug on home page.

--- ChemInv/config.php
+++ ChemInv/config.php
@@ -1,8 +1,6 @@
 <?php
 	define("ROOT_PATH", "./");
 	
-	define("CONFIG_PATH", ROOT_PATH."_config/");
-	
 	require_once("config_pre.php");
 	require_once(CLASSES_PATH."/ChemicalParser.php");
 	require_once("config_post.php");
